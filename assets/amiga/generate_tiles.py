52ae55fa06dc6a02b0b1c5285ba5b937f6e8d689
Jean-François Fabre <jotd666@gmail.com>
2026-09-12 22:19:54 +0200

    added sprite sheets with proper palette

--- assets/amiga/generate_tiles.py
+++ assets/amiga/generate_tiles.py
@@ -71,11 +71,11 @@ def doit(nb_colors,offset,nb_cluts,kind,ref_clut_index,hud=False,dump_it=False):
 def doit_hud_tiles(dump_it=False):
     return doit(8,0,256,"hud",ref_clut_index=0x0,hud=True,dump_it=dump_it)
 def doit_tiles_16x16(dump_it=False):
-    return doit(8,512//8,128,"tiles_16x16",ref_clut_index=0,dump_it=dump_it)
+    return doit(8,0,64,"tiles_16x16",ref_clut_index=0,dump_it=dump_it)
 def doit_sprites_16x16(dump_it=False):
-    return doit(16,256//16,16,"sprites_16x16",ref_clut_index=0,dump_it=dump_it)
+    return doit(16,2048//16,128,"sprites_16x16",ref_clut_index=0,dump_it=dump_it)
 
 if __name__ == "__main__":
-    doit_hud_tiles(True)
-    #doit_tiles_16x16(False)
-    #doit_sprites_16x16(True)
+    #doit_hud_tiles(True)
+    #doit_tiles_16x16(True)
+    doit_sprites_16x16(True)
